30755aa6f871d7d791a32682afc9389944491c0a
skn437 <skn437physx@gmail.com>
2024-01-10 02:26:12 +0600

    Added Time Stdlib Practices

---
+++ src/main.dart
@@ -0,0 +1,3 @@
+void main() {
+  print("Hello Wang So! 👍");
+}
---
+++ src5/pkgs/utils/user.type.go
@@ -0,0 +1,6 @@
+package utils
+
+type UserType struct {
+	Name  string //* You must name the keys in capital letters to make it public
+	Count int
+}
